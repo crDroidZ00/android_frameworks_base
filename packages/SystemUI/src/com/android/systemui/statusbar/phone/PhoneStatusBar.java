c4599412eace277da780f5673ed1784beac94720
Shaun Eaton <shaunsibu@gmail.com>
2015-09-28 16:26:45 +0200

    Add new change for TRDS switch.
    Change-Id: Ibabd5ea27d83bb8ad195ba083078dc7fb36cdfed
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -538,6 +538,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BATTERY_SAVER_MODE_COLOR),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.Secure.UI_THEME_MODE), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.Secure.UI_THEME_AUTO_MODE), false, this,
+                    UserHandle.USER_ALL);                    
             update();
         }
 
@@ -596,6 +602,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         1,
                         UserHandle.USER_CURRENT);
                 setHeadsUpGlobalSwitch(headsUpGlobalSwitch);
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.Secure.UI_THEME_MODE))
+                    || uri.equals(Settings.System.getUriFor(
+                    Settings.Secure.UI_THEME_AUTO_MODE))) {
+                    recreateStatusBar();
+                    updateRowStates();
+                    updateSpeedbump();
+                    updateClearAll();
+                    updateEmptyShadeView();
             }
             update();
         }
@@ -4365,6 +4380,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (uiThemeMode != mCurrUiThemeMode) {
             mCurrUiThemeMode = uiThemeMode;
            // recreateStatusBar(false);
+            updateRowStates();
+            updateSpeedbump();
+            updateClearAll();
+            updateEmptyShadeView();
         } else {
             loadDimens();
         }
